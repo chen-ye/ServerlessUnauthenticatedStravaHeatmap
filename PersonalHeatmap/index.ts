72324604a2ad5fae333f5dbf10e6750054bad87a
Chen Ye <cye@alumni.brown.edu>
2026-05-09 00:25:07 -0700

    latest changes

--- PersonalHeatmap/index.ts
+++ PersonalHeatmap/index.ts
@@ -1,39 +1,55 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import fetch from "node-fetch";
+import fetch, { Response } from "node-fetch";
 import { buffer } from "get-stream";
+import { URL, URLSearchParams } from "url";
 
-const COOKIE_KEY = '_strava4_session';
-const cookieValue = process.env['_strava4_session'];
+const COOKIE_KEY = "_strava4_session";
+const cookieValue = process.env["_strava4_session"];
 const cookieHeader = `${COOKIE_KEY}=${cookieValue}`;
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const {
-        colorType,
-        x,
-        y,
-        z,
-    } = req.params;
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const { colorType, x, y, z } = req.params;
 
-    const reqUrl = `https://personal-heatmaps-external.strava.com/tiles/9652598/${colorType}/${z}/${x}/${y}.png?filter_type=all&filter_start=2012-01-01&filter_end=2099-12-31&include_everyone=true`;
+  const reqUrl = new URL(
+    `https://personal-heatmaps-external.strava.com/tiles/9652598/${colorType}/${z}/${x}/${y}.png?filter_type=all&filter_start=2012-01-01&filter_end=2099-12-31&include_everyone=true`
+  );
+  const queryParams = new URLSearchParams(req.query);
+  queryParams.forEach((value, key) => {
+    reqUrl.searchParams.set(key, value);
+  })
+  const reqUrlString = reqUrl.toString();
 
-    const stravaResponse = await fetch(
-        reqUrl,
-        {
-            headers: {
-                'cookie': cookieHeader,
-            }
-        }
+  context.log(`[Personal Heatmap] url: ${reqUrlString}`);
+
+  const stravaResponse = await fetch(reqUrlString, {
+    headers: {
+      cookie: cookieHeader,
+    },
+  });
+
+  const bodyBuffer = await buffer(stravaResponse.body, { encoding: "binary" });
+
+  if (!stravaResponse.ok) {
+    context.log.error(
+      `[Personal Heatmap] request error: ${bodyBuffer.toString("utf-8")}`
     );
+  }
 
-    const bodyBuffer = await buffer(stravaResponse.body, { encoding: 'binary'});
+  const stravaHeaders = Object.fromEntries(stravaResponse.headers.entries());
 
-    context.res = {
-        status: stravaResponse.status,
-        headers: Object.fromEntries(stravaResponse.headers.entries()),
-        body: new Uint8Array(bodyBuffer),
-        isRaw: true,
-    };
+  context.res = {
+    status: stravaResponse.status,
+    headers: {
+      "content-type": stravaHeaders["content-type"],
+      // prettier-ignore
+      "connection": stravaHeaders["connection"],
+    },
+    body: new Uint8Array(bodyBuffer),
+    isRaw: true,
+  };
 };
 
 export default httpTrigger;
